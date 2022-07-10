9f8d54d162551e4ab54f138b0281568de011d0ed
Surina-Singh <surinsingh@hotmail.co.za>
2022-07-10 04:50:35 -0700

    Task O

--- _printf.c
+++ _printf.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
+ * check_specs - Valid format specifier checker
+ * @format: format specifier
  *
- * Return: pointer to valid function or NULL
+ * Return: If valid function - return pointer. Otherwise NULL
  */
-static int (*check_for_specifiers(const char *format))(va_list)
+static int (*check_specs(const char *format))(va_list)
 {
 	unsigned int i;
 	print_t p[] = {
@@ -29,14 +29,15 @@ static int (*check_for_specifiers(const char *format))(va_list)
 }
 
 /**
- * _printf - prints anything
- * @format: list of argument types passed to the function
+ * _printf - print format
+ * @format: argument types passed to the function
  *
- * Return: number of characters printed
+ * Return: prints number of characters
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
+  unsigned int i = 0;
+	  unsigned int count = 0;
 	va_list valist;
 	int (*f)(va_list);
 
