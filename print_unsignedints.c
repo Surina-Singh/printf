847170204491f037665cc176778182cc84bf0db6
Surina-Singh <surinsingh@hotmail.co.za>
2022-07-10 06:09:44 -0700

    Task3

--- print_unsignedints.c
+++ print_unsignedints.c
@@ -33,3 +33,68 @@ int print_b(va_list b)
 	}
 	return (count);
 }
+
+/**
+ * print_u - Convert an unsigned into binary notation
+ * @u: unsigned integer to convert
+ *
+ * Return: number of digits
+ */
+
+int print_u(va_list u)
+{
+	unsigned int a[10];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(u, unsigned int);
+	m = 1000000000; /* (10 ^ 9) */
+	a[0] = n / m;
+	for (i = 1; i < 10; i++)
+	{
+		m /= 10;
+		a[i] = (n / m) % 10;
+	}
+	for (i = 0, sum = 0, count = 0; i < 10; i++)
+	{
+		sum += a[i];
+		if (sum || i == 9)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * print_o - Convert an unsigned into binary notation
+ * @o: unsigned integer to convert
+ *
+ * Return: number of digits
+ */
+int print_o(va_list o)
+{
+	unsigned int a[11];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(o, unsigned int);
+	m = 1073741824; /* (8 ^ 10) */
+	a[0] = n / m;
+	for (i = 1; i < 11; i++)
+	{
+		m /= 8;
+		a[i] = (n / m) % 8;
+	}
+	for (i = 0, sum = 0, count = 0; i < 11; i++)
+	{
+		sum += a[i];
+		if (sum || i == 10)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
