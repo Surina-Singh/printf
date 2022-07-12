3aa3c6a736178d972d0205aaec1bd9ef63005a24
Bex <ruthrebekahcupido@gmail.com>
2022-07-12 06:19:29 -0700

    Fix integers error

--- print_integers.c
+++ print_integers.c
@@ -8,34 +8,33 @@
 int print_int(va_list i)
 {
 	int a[10];
-	int z, x, y, sum, count;
+	int x, y, z, sum, count;
 
-	x = va_arg(i, int);
+	z = va_arg(i, int);
 	count = 0;
 	y = 1000000000;
-	a[0] = x / y;
-	for (z = 1; z < 10; z++)
+	a[0] = z / y;
+	for (x = 1; x < 10; x++)
 	{
 		y /= 10;
-		a[z] = (x / z) % 10;
+		a[x] = (z / y) % 10;
 	}
-	if (y < 0)
+	if (z < 0)
 	{
 		_putchar('-');
 		count++;
-
-		for (z = 0; z < 10; z++)
-			a[z] *= -1;
+		for (x = 0; x < 10; x++)
+			a[x] *= -1;
 	}
-
-	for (z = 0, sum = 0; z < 10; z++)
+	for (x = 0, sum = 0; x < 10; x++)
 	{
-		sum += a[z];
-		if (sum != 0 || z == 9)
+		sum += a[x];
+		if (sum != 0 || x == 9)
 		{
-			_putchar('0' + a[z]);
+			_putchar('0' + a[x]);
 			count++;
 		}
 	}
 	return (count);
 }
+
