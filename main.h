82f271bc53b2db35ce0c2d57a35022eefeb3222e
Surina-Singh <surinsingh@hotmail.co.za>
2022-07-12 04:23:32 -0700

    print_bin.c

---
+++ main.h
@@ -0,0 +1,33 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * struct print - defines a structure that prints
+ *                various functions and symbols
+ * @t: type
+ * @f: function
+ */
+typedef struct printf
+{
+	char *t;
+	int (*f)(va_list);
+} printf_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list ar_list);
+int print_string(va_list ar_list);
+int print_int(va_list i);
+int print_dec(va_list d);
+int print_bin(va_list b);
+int print_unsigned(va_list u);
+int print_oct(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+
+#endif
