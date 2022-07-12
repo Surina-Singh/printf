0139a5148d0979bb13574729aa4e23db4110561e
Surina-Singh <surinsingh@hotmail.co.za>
2022-07-12 05:10:03 -0700

    updated print_bin

--- print_bin.c
+++ print_bin.c
@@ -7,29 +7,30 @@
  */
 int print_bin(va_list b)
 {
-        unsigned int n, m;
-        unsigned int i = 0;
-        unsigned int sum = 0;
-        unsigned int a[32];
-        int count = 0;
+		unsigned int n, m;
+		unsigned int i = 0;
+		unsigned int sum = 0;
+		unsigned int a[32];
+		int count = 0;
 
-        n = va_arg(b, unsigned int);
-        m = 2147483648;
-        a[0] = n / m;
-        for (i = 1; i < 32; i++)
-        {
-                m /= 2;
-                a[i] = (n / m) % 2;
-        }
+		n = va_arg(b, unsigned int);
+		m = 2147483648;
+		a[0] = n / m;
 
-        for (i = 0, sum = 0, count = 0; i < 32; i++)
-        {
-                sum += a[i];
-                if (sum || i == 31)
-                {
-                        _putchar('0' + a[i]);
-                        count++;
-                }
-        }
-        return (count);
+			for (i = 1; i < 32; i++)
+		{
+				m /= 2;
+				a[i] = (n / m) % 2;
+		}
+
+			for (i = 0, sum = 0, count = 0; i < 32; i++)
+		{
+				sum += a[i];
+					if (sum || i == 31)
+				{
+						_putchar('0' + a[i]);
+						count++;
+				}
+		}
+		return (count);
 }
